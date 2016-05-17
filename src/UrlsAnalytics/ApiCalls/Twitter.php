69ccfcdc76c3209b8e86a64a60077292c20954fd
Ka <kcassam@users.noreply.github.com>
2016-05-17 18:26:13 +0200

    actual code

--- src/UrlsAnalytics/ApiCalls/Twitter.php
+++ src/UrlsAnalytics/ApiCalls/Twitter.php
@@ -13,6 +13,42 @@ class Twitter extends ApiCalls
 {
 
 	protected $apiFormat = "http://tweetcount.io/api/url?url=%s";
+    // exemple http://tweetcount.io/api/url?url=ef0bdd815d2a39741c4c30842b7f9488 on passe uniquement access_token
+
+
+    public function get(array $urls) {
+        $res = array();
+        
+		if (empty($urls)) {
+	    	return false;
+        }
+		array_splice ($urls, $this->limit);
+		
+        foreach ($urls as $url) {
+			echo ".";
+            parse_str(parse_url($url, PHP_URL_QUERY), $output);
+            $query = sprintf($this->apiFormat, $output['access_token']));
+
+            // TODO : Il faudrait plutot utiliser curl à la place de file_get_contents (plus performant, plus propre. Voir littérature web sur le sujet
+            try {
+    			$incoming = @file_get_contents($query);
+            } catch (Exception $e) {
+    			$res[$url] = ['error' => $e];
+    			continue;
+            }
+            if ($incoming === false) {
+                $e = new Exception(sprintf('file_get_contents(%s) is false', $query));
+                $res[$url] = ['error' => $e];
+                continue;
+            }
+
+            $incoming = json_decode($incoming, true);
+			$res[$url] = $incoming;
+		}
+        return $res;
+    }
+
+
 
 	
 }
